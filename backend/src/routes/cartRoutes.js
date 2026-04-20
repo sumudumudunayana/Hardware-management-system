837123cdc487311718a5e3986d260691c323b53f
IT24102743 <IT24102743@my.sliit.lk>
2026-04-20 23:42:58 +0530

    cart routes added

---
+++ backend/src/routes/cartRoutes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getCart,
+  addToCart,
+  updateQty,
+  clearCart,
+  removeItem
+} = require("../controllers/cartController");
+
+const { protect } = require("../middleware/authMiddleware");
+
+// Protect ALL routes
+router.use(protect);
+
+//  Get cart
+router.get("/", getCart);
+
+//  Add item
+router.post("/add", addToCart);
+
+//  Update quantity
+router.put("/update", updateQty);
+
+//  Clear cart
+router.delete("/clear", clearCart);
+
+//  Remove single item
+router.delete("/remove/:itemId", removeItem);
+
+module.exports = router;
